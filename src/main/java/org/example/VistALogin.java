9942860e6ba64bcca90eff80aca21a10b916b9b7
abu0222 <abusayed.mahfuz@va.gov>
2023-01-31 17:46:20 -0500

    Initial commit

---
+++ src/main/java/org/example/VistALogin.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class VistALogin {
+    @io.cucumber.java.en.Given("^I navigate to the login page$")
+    public void iNavigateToTheLoginPage() {
+    }
+
+    @io.cucumber.java.en.And("^I enter the username as admin and password as admin$")
+    public void iEnterTheUsernameAsAdminAndPasswordAsAdmin() {
+    }
+
+    @io.cucumber.java.en.And("^I click login button$")
+    public void iClickLoginButton() {
+    }
+
+    @io.cucumber.java.en.Then("^I should see the userform page$")
+    public void iShouldSeeTheUserformPage() {
+    }
+}
